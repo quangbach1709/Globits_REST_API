a6addfc85d70a13f7914a6d21a5ebf5627468670
quangbach1709 <bachh1124@edumail365.onmicrosoft.com>
2024-12-02 16:30:44 +0700

    Initial commit

---
+++ src/main/java/com/quangbach1709/globits_rest_api/GlobitsRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.quangbach1709.globits_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobitsRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobitsRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quangbach1709/globits_rest_api/GlobitsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangbach1709.globits_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobitsRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
