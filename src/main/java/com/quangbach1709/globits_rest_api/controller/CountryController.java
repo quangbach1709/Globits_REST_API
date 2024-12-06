cc2efb1d70c920165489c5c6c5b3757322d533f1
quangbach1709 <bachh1124@edumail365.onmicrosoft.com>
2024-12-06 16:47:33 +0700

    CRUD cho country

--- src/main/java/com/quangbach1709/globits_rest_api/controller/CountryController.java
+++ src/main/java/com/quangbach1709/globits_rest_api/controller/CountryController.java
@@ -1,16 +1,51 @@
 package com.quangbach1709.globits_rest_api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.quangbach1709.globits_rest_api.model.Country;
+import com.quangbach1709.globits_rest_api.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/counties")
 public class CountryController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello world!";
+    @Autowired
+    private CountryService countryService;
+
+
+    //get all countries
+    @GetMapping("/all")
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
     }
+
+    //create country
+    @PostMapping("/create")
+    public Country createCountry(@RequestBody Country country) {
+        return countryService.createCountry(country);
+    }
+
+    //get country by id
+    @GetMapping("/{id}")
+    public Country getCountryById(@PathVariable int id) {
+        return countryService.getCountryById(id).orElseThrow(
+                () -> new RuntimeException("Country not found with id: " + id)
+        );
+    }
+
+    //update country
+    @PutMapping("/{id}")
+    public Country updateCountry(@PathVariable int id, @RequestBody Country country) {
+        return countryService.updateCountry(id, country);
+    }
+
+    //delete country
+    @DeleteMapping("/{id}")
+    public void deleteCountry(@PathVariable int id) {
+        countryService.deleteCountry(id);
+    }
+
 
 }
